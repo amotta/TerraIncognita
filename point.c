e4b614dff5b031089058a9f10b8bf5693851e041
Alessandro Motta <amotta91@gmail.com>
2011-11-11 16:39:25 +0100

    Delete point_t structure and rewrite functions in point.c

--- point.c
+++ point.c
@@ -1,18 +1,15 @@
 #include "point.h"
 
-bool pointInMap(point_t* point, map_t* map){
-    if(point->row < map->rows && point->col < map->cols){
+bool pointInMap(unsigned int row, unsigned int col, map_t* map){
+    if(row < map->rows && col < map->cols){
         return true;
     }else{
         return false;
     }
 }
 
-bool pointOnBorder(point_t* point, map_t* map){
-    if(
-       point->row == 0 || point->row == map->rows - 1
-       || point->col == 0 || point->col == map->cols - 1
-    ){
+bool pointOnBorder(unsigned int row, unsigned int col, map_t* map){
+    if(row == 0 || col == 0 || row == map->rows - 1 || col == map->cols - 1){
         return true;
     }else{
         return false;
--- point.h
+++ point.h
@@ -1,15 +1,10 @@
 #ifndef TerraIncognita_point_h
 #define TerraIncognita_point_h
 
-typedef struct point{
-    unsigned int row;
-    unsigned int col;
-} point_t;
-
 #include <stdbool.h>
 #include "map.h"
 
-bool pointInMap(point_t*, map_t*);
-bool pointOnBorder(point_t*, map_t*);
+bool pointInMap(unsigned int, unsigned int, map_t*);
+bool pointOnBorder(unsigned int, unsigned int, map_t*);
 
 #endif
