f28f7ba06fdb4c325a67818c162cd03a9feb2d2b
Alessandro Motta <amotta91@gmail.com>
2011-11-14 21:27:53 +0100

    Remove old obstCollide function

--- obstacle.c
+++ obstacle.c
@@ -33,21 +33,6 @@ bool obstOnBorder(obst_t* obst, map_t* map){
     }
 }
 
-bool obstCollides(obst_t* obst, map_t* map){
-    unsigned int r;
-    unsigned int c;
-    
-    for(r = obst->top - 1; r <= obst->bottom + 1; r++){
-        for(c = obst->left - 1; c <= obst->right + 1; c++){
-            if(mapGet(map, r, c) == FIELD_OBSTACLE){
-                return true;
-            }
-        }
-    }
-    
-    return false;
-}
-
 void obstSetInit(obstSet_t* set){
     set->set = (obst_t*) malloc(set->length * sizeof(obst_t));
 }
